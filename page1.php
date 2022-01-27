b3dc9b5fe6f65183ef5423ebc33ae7a2d74de01e
Praiya31 <97584432+Praiya31@users.noreply.github.com>
2022-01-27 09:09:17 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -2,9 +2,9 @@
   <head>
     <title>งานที่ 1</title>
   </head>
-  <body bgcolor="light-blue">
+  <body bgcolor="lightblue">
     page1
-  <h1 style="color:dark-blue;">ประวัติส่วนตัว</h1>
+  <h1 style="color:darkblue;">ประวัติส่วนตัว</h1>
   <b> ชื่อ </b> <i> นางสาวไปรยา ลีปรีย์ธนกิจ </i>
   <b> ชื่อเล่น </b> <i> ไข่มุก </i>
   <b> ชั้น </b> <i> ม.4/12 </i>
