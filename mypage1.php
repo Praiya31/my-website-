1404030f514633fc8e6d35070b49f579904e1c29
Praiya31 <97584432+Praiya31@users.noreply.github.com>
2022-02-09 16:05:48 +0700

    Create mypage1.php

---
+++ mypage1.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>เว็บไซต์ของไปรยา ลีปรีย์ธนกิจ</title>
+  </head>
+  <body>
+    <a href="https://facebook.com"><h1>Facebook</h1></a>
+    <a href="mypage2.php"><h1>Mypage2</h1></a>
+  </body>
+</html>
