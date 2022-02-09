29c700bf78e5f8f7edfe4ae64244b3ae5c00cf5e
Praiya31 <97584432+Praiya31@users.noreply.github.com>
2022-02-09 14:24:48 +0700

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>การเชื่อมโยงเว็บไซต์</title>
+  </head>
+  <body>
+    <a href="https://lomsak.ac.th"><h1>วาร์ปของฉัน</h1></a>
+    <a href="contact.php"><h1>ติดต่อฉัน</h1></a>
+  </body>
+</html>
