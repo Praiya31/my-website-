90a26d573854818e427d7a0c291e29fd8ef77d0e
Praiya31 <97584432+Praiya31@users.noreply.github.com>
2022-03-02 16:04:40 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -3,11 +3,42 @@
     <title>การสร้างตาราง</title>
   </head>
   <body>
-    <table>
+    <table border="1">
       <tr>
-        <td>คอลัมน์ที่ 1</td>
-        <td>คอลัมน์ที่ 2</td>
-        <td>คอลัมน์ที่ 3</td>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/FB</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>จีซู แบล็คพิ้งค์</td>
+        <td><img src="https://www.google.co.th/url?sa=i&url=https%3A%2F%2Fwww.thaiticketmajor.com%2Fvariety%2Flifestyle%2F14087%2F&psig=AOvVaw2Mi9fJjFFdisBp7a21HU21&ust=1646296968245000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJDJ3qSEp_YCFQAAAAAdAAAAABAJ" width="45px"></td>
+        <td><a href="#https://instagram.com/sooyaaa__?utm_medium=copy_link">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>เจนนี่ แบล็คพิ้งค์</td>
+        <td><img src="https://www.google.co.th/url?sa=i&url=https%3A%2F%2Fwww.pptvhd36.com%2Fnews%2F%25E0%25B8%2582%25E0%25B9%2588%25E0%25B8%25B2%25E0%25B8%25A7%25E0%25B8%259A%25E0%25B8%25B1%25E0%25B8%2599%25E0%25B9%2580%25E0%25B8%2597%25E0%25B8%25B4%25E0%25B8%2587%2F142699&psig=AOvVaw0mK4zH81vVWXfIJqZhuqVZ&ust=1646297145511000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLCwwvmEp_YCFQAAAAAdAAAAABAOc" width="45px"></td>
+        <td><a href="https://instagram.com/jennierubyjane?utm_medium=copy_link">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>#ลิซ่า แบล็คพิ้งค์</td>
+        <td><img src="https://www.google.co.th/url?sa=i&url=https%3A%2F%2Fwww.matichon.co.th%2Fentertainment%2Fnews_2174626&psig=AOvVaw2NRVueXXRdSGeCW4YZmcyt&ust=1646297822011000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNCVpLuHp_YCFQAAAAAdAAAAABAD" width="45px"></td>
+        <td><a href="https://instagram.com/lalalalisa_m?utm_medium=copy_link">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>โรเซ่ แบล็คพิ้งค์</td>
+        <td><img src="https://www.google.co.th/url?sa=i&url=https%3A%2F%2Fwww.gqthailand.com%2Fstyle%2Farticle%2Frose-blackpink-tiffany-and-co&psig=AOvVaw1QP8JTOU5GnLEAJPKyJL7U&ust=1646297954560000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNjyyfyHp_YCFQAAAAAdAAAAABAn" width="45px"></td>
+        <td><a href="https://instagram.com/roses_are_rosie?utm_medium=copy_link">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>ฮเยริ</td>
+        <td><img src="https://www.google.co.th/url?sa=i&url=https%3A%2F%2Fwww.trueplookpanya.com%2Fblog%2Fcontent%2F89399%2F-enttv-ent-&psig=AOvVaw1SPJ3tGE-5Lh10CFxAb219&ust=1646298188635000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNDO-OmIp_YCFQAAAAAdAAAAABAJ" width="45px"></td>
+        <td><a href="https://instagram.com/hyeri_0609?utm_medium=copy_link">Facebook</a></td>
       </tr>
     </table>
   </body>
